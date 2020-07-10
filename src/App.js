cb6d47e3bca05d7fea1cb1344fa37415ee37520b
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-07-10 16:43:55 -0500

    Adds footer component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from './components/Header';
 import ArtObject from './components/ArtObject';
 import './App.css';
 import art from './art.json';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ArtObject art={art[0]} />
-      <ArtObject art={art[1]} />
+      <main>
+        <ArtObject art={art[0]} />
+        <ArtObject art={art[1]} />
+      </main>
+      <Footer />
     </div>
   );
 }
