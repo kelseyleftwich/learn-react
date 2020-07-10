95022e0d14782ffec525dafe177e8b86d17240f9
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-07-10 17:20:49 -0500

    Adds filtering by featured field

--- src/components/ArtObjectList.js
+++ src/components/ArtObjectList.js
@@ -4,21 +4,61 @@ import ArtObject from './ArtObject';
 import types from '../types/propTypes';
 import { Component } from 'react';
 
+const style = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
 class ArtObjectList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      featuredOnly: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      featuredOnly: !state.featuredOnly,
+    }));
   }
 
   render() {
     const { artObjects } = this.props;
-    return artObjects.map((artObject) => (
-      <ArtObject
-        key={artObject.objectNumber}
-        artObject={artObject}
-        featured={artObject.featured}
-      />
-    ));
+    const { featuredOnly } = this.state;
+
+    const filteredArtObjects = !featuredOnly
+      ? artObjects
+      : artObjects.filter((artObject) => artObject.featured);
+
+    return (
+      <section>
+        <header style={style}>
+          <div>
+            <p>
+              {filteredArtObjects.length} {featuredOnly && 'Featured '}Objects
+            </p>
+          </div>
+
+          <div>
+            <button onClick={this.handleClick}>
+              {featuredOnly ? 'Show All' : 'Show Featured Only'}
+            </button>
+          </div>
+        </header>
+        {filteredArtObjects.map((artObject) => (
+          <ArtObject
+            key={artObject.objectNumber}
+            artObject={artObject}
+            featured={artObject.featured}
+          />
+        ))}
+      </section>
+    );
   }
 }
 
