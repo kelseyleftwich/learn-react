9ffa1cb4addde1a92c3819fa457643136c69f2dd
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-07-11 07:56:18 -0500

    Extracts out Gallery component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import art from './art.json';
 import Footer from './components/Footer';
-import ArtObjectList from './components/ArtObjectList';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <ArtObjectList artObjects={art} />
-      </main>
+      <Gallery />
       <Footer />
     </div>
   );
