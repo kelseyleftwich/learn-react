ddaa0cf4ca13216f21defb4a74953bbec14963ec
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-07-11 08:07:28 -0500

    Adds about page, router, and nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import './App.css';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Gallery />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Gallery />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="App-header">
-      <hgroup>
-        <h1>Harvard Art Museums</h1>
-        <h2>Fogg Museum / Busch-Reisinger Museum / Arthur M. Sackler Museum</h2>
-      </hgroup>
-    </header>
+    <div className="App-header">
+      <header>
+        <hgroup>
+          <h1>Harvard Art Museums</h1>
+          <h2>
+            Fogg Museum / Busch-Reisinger Museum / Arthur M. Sackler Museum
+          </h2>
+        </hgroup>
+      </header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
   );
 };
 
